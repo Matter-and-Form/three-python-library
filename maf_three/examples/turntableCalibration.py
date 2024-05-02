1a9e8c5d7199a95c0cbb40c237b4aa2102122c98
David <david@matterandform.net>
2024-05-02 17:17:48 +0200

    Clean up

--- maf_three/examples/turntableCalibration.py
+++ maf_three/examples/turntableCalibration.py
@@ -8,36 +8,39 @@ from maf_three.V3Task import V3Task
 
 
 done = False
-cornersDetected_0 = 0
-cornersDetected_1 = 0
-cornersTotal = 0
+cornersDetected_0 = 0 # Amount of corners detected on camera 0
+cornersDetected_1 = 0 # Amount of corners detected on camera 1
+cornersTotal = 0 # Total number of corners to be detected
 
 def main():
 
     def OnTask(task:Task):
         global done
-        if task.Type == V3Task.CalibrateTurntable:
 
-            # Calibration progress
+        # Calibrate turntable task
+        if task.Type == V3Task.CalibrateTurntable:
+            # Task progress
             if task.Progress != None:
                 progress = task.Progress['CalibrateTurntable']
                 print(progress['current'] , '/', progress['total'], '-', progress['step'])
 
+            # Task completed ?
             if task.State == TaskState.Completed:
                 print('Calibration Completed')
+                print(task.Output)
                 done = True
+            # Task failed ?
             elif task.State == TaskState.Failed:
                 print('Calibration Failed')
-                print('Error:', task.Error)
+                print('Calibration error:', task.Error)
                 done = True
-
-
 
     def OnBuffer(descriptor, buffer:bytes):
         global cornersDetected_0, cornersDetected_1, cornersTotal
 
-        # Video task
+        # Video task ?
         if descriptor['Task']['Index'] == -1:
+            # Calibration card present in the descriptor
             if "calibrationCard" in descriptor['Descriptor']:
                 calibrationCard = descriptor['Descriptor']['calibrationCard']
                          
@@ -47,13 +50,16 @@ def main():
                     cardHeight = calibrationCard['size'][1]
                     cornersTotal = (cardWidth - 1) * (cardHeight - 1)
 
-                # Camera 0
+                
                 detectedCorners = len(calibrationCard['corners'])
+                # Camera 0
                 if descriptor['Index'] == 0:
                     cornersDetected_0 = detectedCorners
+                # Camera 1
                 else:
                     cornersDetected_1 = detectedCorners              
             
+            # No calibration card in the descriptor
             else:
                 if descriptor['Index'] == 0:
                     cornersDetected_0 = 0
@@ -71,7 +77,7 @@ def main():
         scanner.SendTask(-1, V3Task.StartVideo)   
 
         # Detect the calibration card
-        print('Detecting the calibration card')
+        print('******* Detecting the calibration card')
         scanner.SendTask(0, V3Task.DetectCalibrationCard, 3)
 
         # Wait for the calibration card to be detected
@@ -86,7 +92,7 @@ def main():
         time.sleep(1)
 
         # Calibration the turntable
-        print('\nCalibrating the turntable')
+        print('\n******* Calibrating the turntable')
         scanner.SendTask(1, V3Task.CalibrateTurntable)
 
         # Wait for the tasks to finish
