e01e7c1ee0fa369ce9c10e97bdce8809d479b963
David <david@matterandform.net>
2024-05-03 11:44:56 +0200

    USM ; Projector only uses proto objects

--- maf_three/json.py
+++ maf_three/json.py
@@ -3,14 +3,22 @@
 from array import *
 import json
 
+from google.protobuf.json_format import MessageToDict
+
 
 def Serializer(object): 
     # Array is not JSON serializable => Convert to list
     if isinstance(object, array):
         return object.tolist()
-        
-    return dict(filter(lambda tup:tup[1] is not None, object.__dict__.items()))
     
+    # Protobuf object
+    if hasattr(object, "DESCRIPTOR"):
+        dic = MessageToDict(object, preserving_proto_field_name=True)
+        return dict(filter(lambda tup:tup[1] is not None, dic.items()))
+    
+    # Our objects
+    return dict(filter(lambda tup:tup[1] is not None, object.__dict__.items()))
+
 
 def TO_JSON(object) -> str:
     return json.dumps(
