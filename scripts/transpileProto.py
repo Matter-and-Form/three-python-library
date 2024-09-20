1741350f9b4e90cbc6377564ee384753605f05d8
Drew Shark <drew@matterandform.net>
2024-09-19 21:28:31 -0400

    Added launch.json. Moved tree to seperate file. Using Tree instead of Message Types

--- scripts/transpileProto.py
+++ scripts/transpileProto.py
@@ -2,161 +2,46 @@ import os
 import argparse
 from interpretProto import create_proto_objects, MessageType, parse_proto
 from typing import List, Dict,Tuple, Set
+from enum import Enum
+from tree import TreeNode, Tree, TreeProperty, NodeType, ImportDescriptor, get_descriptor_by_name, get_descriptor_by_partial_name
 
 
-class TreeProperty:
-    def __init__(self, type_: str, name: str, optional: bool, comment: str, repeated) -> None:
-        self.type: str = type_
-        self.name: str = name
-        self.optional: bool = optional
-        self.repeated: bool = repeated
-        self.comment: str = comment
+# Mapping of special types to Python types
+type_mapping = {
+    "int32": "int",
+    "Int32": "int",
+    "int64": "int",
+    "Int64": "int",
+    "uint64": "int",
+    "UInt64": "int",
+    "uint32": "int",
+    "UInt32": "int",
+    "bool": "bool",
+    "Bool": "bool",
+    "float": "float",
+    "Float": "float",
+    "double": "float",
+    "Double": "float",
+    "string": "str",
+    "String": "str",
+    "google.protobuf.Any": "_any_pb2"
+}
 
-class TreeNode:
-    def __init__(self, name: str, parent = None, filespace = None):
-        self.name = name
-        self.parent = parent
-        self.children = {}
-        self.properties = []
-        self.imports = []
-        self.filespace = filespace
-
-    def add_child(self, child_node):
-        self.children[child_node.name] = child_node
-    
-    def add_property(self, type_: str, name: str, optional: bool, comment: str, repeated: bool) -> None:
-        self.properties.append(TreeProperty(type_, name, optional, comment, repeated))
-    
-    def add_import(self, import_path: str):
-        self.imports.append(ImportDescriptor(import_path))
-
-    def get_child(self, name: str):
-        parts = name.split('.')
-        # Check each part of the name to see if it is a child
-        temp = self
-        while temp and len(parts) > 0:
-            temp = temp.children.get(parts[0], None)
-            parts = parts[1:]
-        if temp:
-            return temp
-        return None
-    
-    def has_child(self, name: str):
-        temp = self.get_child(name)
-        if temp:
-            return True
-        return False
-
-    def get_path(self):
-        path = []
-        current_node = self
-        while current_node:
-            if current_node.name == "root":
-                break
-            path.append(current_node.name)
-            current_node = current_node.parent
-        return '.'.join(reversed(path))
-    
-    def get_relative_path(self, node):
-        path = []
-        current_node = self
-        while current_node != node:
-            if current_node.name == "root":
-                break
-            path.append(current_node.name)
-            current_node = current_node.parent
-        if current_node:
-            path.append(current_node.name)
-        return '.'.join(reversed(path))
-    
-    def get_first_parent_with_name(self, name: str):
-        # Break the name into parts
-        top_name = name.split('.')[0]
-
-        current_node = self
-        while current_node:
-            if current_node.name == "root":
-                break
-            if current_node.name == top_name:
-                return current_node
-            current_node = current_node.parent
-        return None
-    
-    def climbing_search(self, name: str):
-        current_node = self
-        #find the top level node and return it's child
-        while current_node:
-            if current_node.name == "root":
-                break
-            if current_node.name == name:
-                return current_node
-            elif current_node.has_child(name):
-                return current_node.get_child(name)
-            current_node = current_node.parent
-        return None
-    
-class Tree:
-    def __init__(self):
-        self.root = TreeNode("root")
-
-    def add_path(self, path: str, filespace) -> TreeNode:
-        parts = path.split('.')
-        current_node = self.root
-        for part in parts:
-            if part not in current_node.children:
-                new_node = TreeNode(part, parent=current_node)
-                current_node.add_child(new_node)
-            current_node = current_node.get_child(part)
-        current_node.filespace = filespace
-        return current_node
-
-    def search(self, path: str) -> TreeNode:
-        parts = path.split('.')
-        current_node = self.root
-        for part in parts:
-            current_node = current_node.get_child(part)
-            if current_node is None:
-                return None
-        return current_node
-    
-    def get_nodes_with_filespace(self, filespace: str) -> TreeNode:
-        nodes = []
-        def get_nodes(node):
-            if node.filespace == filespace:
-                nodes.append(node)
-            for child in node.children.values():
-                get_nodes(child)
-        get_nodes(self.root)
-        return nodes
-
-class ImportDescriptor:
-    def __init__(self, file:str):
-        self.file = file
-        self.types: List[dict[str,str]] = []
-    def add_type(self, type:str, replacement:str):
-        # Check to see if the type is already in the list
-        for t in self.types:
-            if t["type"] == type:
-                return
-        self.types.append({"type":type, "replacement":replacement})
-
-def get_descriptor_by_name(name: str, descriptors: List[dict]) -> ImportDescriptor:
-    for descriptor in descriptors:
-        if descriptor.file == name:
-            return descriptor
-    return None
-
-def get_descriptor_by_partial_name(name, import_descriptors, tree) -> ImportDescriptor:
-    for descriptor in import_descriptors:
-        descriptor_file_nodes = tree.get_nodes_with_filespace(descriptor.file)
-        for descriptor_node in descriptor_file_nodes:
-            if descriptor_node.name == name:
-                return descriptor
-    return None
 
 def generate_python_code(proto_objects: List, output_dir: str, tree:Tree) -> set:
     # create a unique set of paths
     paths = set()
+      
+    branches = tree.get_branches_by_filespace()
+    
+    for branch in branches.values():
+        # First get imports
+        imports = set()
+        for node in branch:
+            imports.update(node.imports)
+        
+        code = generate_import_lines(imports)
+
 
     for obj in proto_objects:
         print(f"Parsing file: {obj['filename']}")
@@ -187,16 +72,16 @@ def generate_python_code(proto_objects: List, output_dir: str, tree:Tree) -> set
 
         # Generate code for Services
         service_code = ""
-        for service in services:
-            code = generate_service_code(service, tree, file_node, importDescs)
-            service_code += code + "\n\n"
+        # for service in services:
+        #     code = generate_service_code(service, tree, file_node, importDescs)
+        #     service_code += code + "\n\n"
             
 
         # Generate code for messages
         message_code = ""
         for message in messages:
             current_node = file_node.get_child(message.name)
-            code = generate_message_code(message, tree, current_node, importDescs)
+            code = generate_class_code(tree, current_node, importDescs)
             message_code += code + "\n\n"
 
         # Generate imports another time to get the final output of imports
@@ -274,7 +159,12 @@ def get_tree(proto_objects: List)-> Tree:
                 nested_name_space = f"{nested_name_space}.{message.name}"
                 #convert filename to namespace 
                 node = tree.add_path(nested_name_space, filespace)
+                if message.type == "enum":
+                    node.type = NodeType.Enum
+                elif message.type == "message":
+                    node.type = NodeType.Class
                 node.imports = importDescs
+                node.comment = parseComment(message.comment)
                 for nested in message.nested_messages:
                     get_nested_messages(nested, nested_name_space)
             get_nested_messages(msg, namespace)
@@ -282,7 +172,9 @@ def get_tree(proto_objects: List)-> Tree:
             # I think services are top level definitions, so no parent needed
             service_path = f"{namespace}.{service.name}"
             node = tree.add_path(service_path, filespace)
+            node.type = NodeType.Service
             node.imports = importDescs
+            node.comment = parseComment(service.comment)
         
     for obj in proto_objects:
         namespace = obj['namespace']
@@ -292,39 +184,20 @@ def get_tree(proto_objects: List)-> Tree:
                 node = tree.search(message.path)
                 for prop in message.properties:
                     prop_type = get_property_type(prop, tree, node, node.imports, namespace)
-                    node.add_property(prop_type, prop.name, prop.optional, prop.comment, prop.repeated)
+                    node.add_property(prop_type, prop.name, prop.optional, parseComment(prop.comment), prop.repeated)
                 for nested in message.nested_messages:
                     parse_message_props(nested)
 
             parse_message_props(msg)
     return tree
 
-# Mapping of special types to Python types
-type_mapping = {
-    "int32": "int",
-    "Int32": "int",
-    "int64": "int",
-    "Int64": "int",
-    "uint64": "int",
-    "UInt64": "int",
-    "uint32": "int",
-    "UInt32": "int",
-    "bool": "bool",
-    "Bool": "bool",
-    "float": "float",
-    "Float": "float",
-    "double": "float",
-    "Double": "float",
-    "string": "str",
-    "String": "str",
-    "google.protobuf.Any": "_any_pb2"
-}
-
 def parseComment(comment: str) -> str:
     if comment != "":
         if len(comment.split('\n')) > 1:
             class_code = f'"""{comment}"""\n'
         else:
+            # remove initial whitespace in comment
+            comment = comment.strip()
             class_code = f'# {comment}\n'
     else:
         class_code = '\n'
@@ -411,27 +284,20 @@ def get_property_type(property, tree: Tree, node:TreeNode, import_descriptors: L
     raise Exception("Property Type could not be resolved", property.type)
     
 
-def generate_message_code(message: Dict, tree: Tree, current_node:TreeNode, import_descriptors: List[ImportDescriptor]) -> str:
+def generate_class_code(tree: Tree, current_node:TreeNode, import_descriptors: List[ImportDescriptor]) -> str:
 
-    if message.type == "enum":
-        return generate_enum_code(message) + "\n\n"
+    if current_node.type == NodeType.Enum:
+        return generate_enum_code(current_node) + "\n\n"
 
-    comment = message.comment
-    name = message.name
-    properties = message.properties
-    nested_messages = message.nested_messages
-    message_namespace = message.namespace
+    properties = current_node.properties
     
-    class_code = parseComment(comment)
+    class_code = current_node.comment
     
-    class_code += f"class {name}:\n"
+    class_code += f"class {current_node.name}:\n"
 
     # Generate code for nested messages
-    for nested_message in nested_messages:
-        if nested_message.type == "enum":
-            nested_class_code = generate_enum_code(nested_message) + "\n\n"
-        else :
-            nested_class_code = generate_message_code(nested_message, tree, current_node.get_child(nested_message.name), import_descriptors)
+    for child in current_node.children.values():
+        nested_class_code = generate_class_code(tree, child, import_descriptors)
         nested_class_code = add_indents(nested_class_code,1)
         class_code += f"\n{nested_class_code}\n"
     
@@ -442,7 +308,6 @@ def generate_message_code(message: Dict, tree: Tree, current_node:TreeNode, impo
         class_code += "    def __init__(self"
         for prop in properties:
             
-            prop_type = get_property_type(prop, tree, current_node, import_descriptors, message_namespace)
 
             # handle repeated
             if prop.repeated:
@@ -452,17 +317,17 @@ def generate_message_code(message: Dict, tree: Tree, current_node:TreeNode, impo
                     descriptor = ImportDescriptor("typing")
                     import_descriptors.append(descriptor)
                 descriptor.add_type("List", "")
-                class_code += f", {prop.name}:List[{prop_type}]"
+                class_code += f", {prop.name}:List[{prop.type}]"
             else:
-                class_code += f", {prop.name}:{prop_type}"
+                class_code += f", {prop.name}:{prop.type}"
             # handle optionals
             if prop.optional:
                 class_code += " = None"
         class_code += "):\n"
         for prop in properties:
-            # Add comments with parseComment function with spaces
+            # Add comments with spaces
             if prop.comment:
-                class_code += f"\n{add_indents(parseComment(prop.comment),2)}"
+                class_code += f"\n{add_indents(prop.comment,2)}"
             class_code += add_indents(f"self.{prop.name} = {prop.name}\n",2)
     else:
         class_code += "    def __init__(self):\n"
@@ -474,25 +339,20 @@ name_mapping = {
     "None": "Empty",
 }
 
-def generate_enum_code(enum) -> str:
-    comment = enum.comment
-    name = enum.name
-    values = enum.properties
-
-    enum_code = parseComment(comment)
-    enum_code += f"class {name}(Enum):\n"
-    for value in values:
+def generate_enum_code(enum:TreeNode) -> str:    
+    enum_code = enum.comment
+    enum_code += f"class {enum.name}(Enum):\n"
+    for value in enum.properties:
         name = name_mapping.get(value.name, value.name)
-        enum_code += f"    {name} = \"{value.name}\"  # {value.comment}\n"
+        enum_code += f"    {name} = \"{value.name}\" {value.comment}\n"
     return enum_code
 
 def generate_service_code(service, tree: Tree, current_node:TreeNode, import_descriptors: List[ImportDescriptor]) -> str:
-    comment = service.comment
     name = service.name
     procedures = service.procedures
     service_namespace = service.namespace
 
-    class_code = parseComment(comment)
+    class_code = service.comment
     class_code += f"class {name}:\n"
     for procedure in procedures:
         comment = procedure.comment
@@ -507,7 +367,7 @@ def generate_service_code(service, tree: Tree, current_node:TreeNode, import_des
             if c.isupper() and i != 0:
                 method_name += "_"
             method_name += c.lower()
-        class_code += parseComment(comment)
+        class_code += comment
         
         # Add the descriptor type so that it can be imported
         assert descriptor != None, f"Descriptor not found for {name}"
---
+++ scripts/tree.py
@@ -0,0 +1,184 @@
+from enum import Enum
+from typing import List, Dict
+
+class TreeProperty:
+    def __init__(self, type_: str, name: str, optional: bool, comment: str, repeated) -> None:
+        self.type: str = type_
+        self.name: str = name
+        self.optional: bool = optional
+        self.repeated: bool = repeated
+        self.comment: str = comment
+
+class NodeType(Enum):
+    Class = "Class"
+    Enum = "Enum"
+    Service = "Service"
+    Directory = "Directory"
+
+class TreeNode:
+    def __init__(self, name: str, nodeType: NodeType = NodeType.Directory, parent = None, filespace = None):
+        self.name = name
+        self.type = nodeType
+        self.parent = parent
+        self.children = {}
+        self.properties = []
+        self.imports = []
+        self.filespace = filespace
+        self.comment = None
+
+    def add_child(self, child_node):
+        self.children[child_node.name] = child_node
+    
+    def add_property(self, type_: str, name: str, optional: bool, comment: str, repeated: bool) -> None:
+        self.properties.append(TreeProperty(type_, name, optional, comment, repeated))
+    
+    def add_import(self, import_path: str):
+        self.imports.append(ImportDescriptor(import_path))
+
+    def get_child(self, name: str):
+        parts = name.split('.')
+        # Check each part of the name to see if it is a child
+        temp = self
+        while temp and len(parts) > 0:
+            temp = temp.children.get(parts[0], None)
+            parts = parts[1:]
+        if temp:
+            return temp
+        return None
+    
+    def has_child(self, name: str):
+        temp = self.get_child(name)
+        if temp:
+            return True
+        return False
+
+    def get_path(self):
+        path = []
+        current_node = self
+        while current_node:
+            if current_node.name == "root":
+                break
+            path.append(current_node.name)
+            current_node = current_node.parent
+        return '.'.join(reversed(path))
+    
+    def get_relative_path(self, node):
+        path = []
+        current_node = self
+        while current_node != node:
+            if current_node.name == "root":
+                break
+            path.append(current_node.name)
+            current_node = current_node.parent
+        if current_node:
+            path.append(current_node.name)
+        return '.'.join(reversed(path))
+    
+    def get_first_parent_with_name(self, name: str):
+        # Break the name into parts
+        top_name = name.split('.')[0]
+
+        current_node = self
+        while current_node:
+            if current_node.name == "root":
+                break
+            if current_node.name == top_name:
+                return current_node
+            current_node = current_node.parent
+        return None
+    
+    def climbing_search(self, name: str):
+        current_node = self
+        #find the top level node and return it's child
+        while current_node:
+            if current_node.name == "root":
+                break
+            if current_node.name == name:
+                return current_node
+            elif current_node.has_child(name):
+                return current_node.get_child(name)
+            current_node = current_node.parent
+        return None
+    
+class Tree:
+    def __init__(self):
+        self.root = TreeNode("root")
+
+    def add_path(self, path: str, filespace) -> TreeNode:
+        parts = path.split('.')
+        current_node = self.root
+        for part in parts:
+            if part not in current_node.children:
+                new_node = TreeNode(part, parent=current_node)
+                current_node.add_child(new_node)
+            current_node = current_node.get_child(part)
+        current_node.filespace = filespace
+        return current_node
+
+    def search(self, path: str) -> TreeNode:
+        parts = path.split('.')
+        current_node = self.root
+        for part in parts:
+            current_node = current_node.get_child(part)
+            if current_node is None:
+                return None
+        return current_node
+    
+    def get_nodes_with_filespace(self, filespace: str) -> TreeNode:
+        nodes = []
+        def get_nodes(node):
+            if node.filespace == filespace:
+                nodes.append(node)
+            for child in node.children.values():
+                get_nodes(child)
+        get_nodes(self.root)
+        return nodes
+    
+    def get_branches_by_filespace(self) -> Dict[str, List[TreeNode]]:
+        """
+        Traverse the tree and collect nodes by their filespace.
+
+        Returns:
+            Dict[str, List[TreeNode]]: A dictionary where the keys are filespace
+            identifiers and the values are lists of TreeNode objects that belong
+            to that filespace.
+        """
+        branch_nodes = {}
+
+        def traverse_for_filespace(node: TreeNode):
+            if node.filespace:
+                if node.filespace not in branch_nodes:
+                    branch_nodes[node.filespace] = []
+                branch_nodes[node.filespace].append(node)
+            else:
+                for child in node.children.values():
+                    traverse_for_filespace(child)
+
+        traverse_for_filespace(self.root)
+
+        return branch_nodes
+    
+class ImportDescriptor:
+    def __init__(self, file:str):
+        self.file = file
+        self.types: List[dict[str,str]] = []
+    def add_type(self, type:str, replacement:str):
+        # Check to see if the type is already in the list
+        for t in self.types:
+            if t["type"] == type:
+                return
+        self.types.append({"type":type, "replacement":replacement})
+
+def get_descriptor_by_name(name: str, descriptors: List[dict]) -> ImportDescriptor:
+    for descriptor in descriptors:
+        if descriptor.file == name:
+            return descriptor
+    return None
+
+def get_descriptor_by_partial_name(name, import_descriptors, tree) -> ImportDescriptor:
+    for descriptor in import_descriptors:
+        descriptor_file_nodes = tree.get_nodes_with_filespace(descriptor.file)
+        for descriptor_node in descriptor_file_nodes:
+            if descriptor_node.name == name:
+                return descriptor
+    return None
