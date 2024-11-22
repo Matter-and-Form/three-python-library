a4c2567149177893fc6f3a4ef72b1ed8e9133e87
drewsipher <drew@matterandform.net>
2024-11-21 20:25:33 -0500

    removed old maf_three

--- maf_three/MF/V3/Settings/Viewer.py
+++
@@ -1,7 +0,0 @@
-class Viewer:
-    # 3D Viewer settings.
-    def __init__(self, textureOpacity: float = None):
-        # Texture opacity.
-        self.textureOpacity = textureOpacity
-
-
--- maf_three/MF/V3/__init__.py
+++
@@ -1,3 +0,0 @@
-from MF.V3.Buffer import * 
-from MF.V3.Task import * 
-from MF.V3.Three import * 
